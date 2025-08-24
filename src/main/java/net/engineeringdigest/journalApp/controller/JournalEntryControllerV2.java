64d6575e837e20537ecb36b6c647b855f6ecfe37
Ridham Gupta <Ridhamkth@gmail.com>
2025-08-24 12:27:38 +0530

    feat: Add User entity, link with JournalEntry, implement CRUD APIs and service layer for business logic

--- src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryControllerV2.java
@@ -2,38 +2,41 @@ package net.engineeringdigest.journalApp.controller;
 
 
 import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.entity.User;
 import net.engineeringdigest.journalApp.service.JournalEntryService;
+import net.engineeringdigest.journalApp.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.*;
 
 @RestController
 @RequestMapping("/journal")
 public class JournalEntryControllerV2 {
 
-    @Autowired JournalEntryService journalEntryService;
+    @Autowired
+    private JournalEntryService journalEntryService;
 
+    @Autowired
+    private UserService userService;
 
-
-    @GetMapping
-    public ResponseEntity<?> getAll(){
-        List<JournalEntry>all = journalEntryService.getAll();
+    @GetMapping("{userName}")
+    public ResponseEntity<?> getAllJournalEntriesOfUser(@PathVariable String userName){
+        User user = userService.findByUserName(userName);
+        List<JournalEntry>all = user.getJournalEntries();
         if(all!=null && !all.isEmpty()){
             return new ResponseEntity<>(all, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping
-    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry){
+    @PostMapping("{userName}")
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry, @PathVariable String userName){
         try{
-            myEntry.setDate(LocalDateTime.now());
-            journalEntryService.saveEntry(myEntry);
+            journalEntryService.saveEntry(myEntry,userName);
             return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
         }
         catch(Exception e){
@@ -52,21 +55,23 @@ public class JournalEntryControllerV2 {
     }
 
 
-    @DeleteMapping("id/{myId}")
-    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId myId){
-        journalEntryService.deleteById(myId );
+    @DeleteMapping("id/{userName}/{myId}")
+    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId myId, @PathVariable String userName ){
+        journalEntryService.deleteById(myId, userName );
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping("/id/{id}")
-    public ResponseEntity<?> updateJournalById(@PathVariable ObjectId id, @RequestBody JournalEntry newEntry){
-        JournalEntry old = journalEntryService.findById(id).orElse(null);
+    @PutMapping("/id/{userName}/{myId}")
+    public ResponseEntity<?> updateJournalById(@PathVariable ObjectId myId, @RequestBody JournalEntry newEntry, @PathVariable String userName){
+
+        JournalEntry old = journalEntryService.findById(myId).orElse(null);
         if(old!=null){
             old.setTitle( newEntry.getTitle()!=null && !newEntry.getTitle().equals("")? newEntry.getTitle(): old.getTitle());
             old.setContent(newEntry.getContent()!=null && !newEntry.getContent().equals("")? newEntry.getContent() : old.getContent() );
-            journalEntryService.saveEntry(old);
+            journalEntryService.saveEntry(old  );
             return new ResponseEntity<>(old, HttpStatus.OK );
         }
+
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
--- src/main/java/net/engineeringdigest/journalApp/entity/User.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/User.java
@@ -6,11 +6,16 @@ import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection="users")
 @Data
 public class User {
+
     @Id
     private ObjectId id;
 
@@ -19,5 +24,7 @@ public class User {
     private String userName;
     @NonNull
     private String passward;
+    @DBRef
+    private List<JournalEntry> journalEntries= new ArrayList<>();
 
 }
