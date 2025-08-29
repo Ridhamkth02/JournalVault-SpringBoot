e4c46f6bbb0be76238a349aa99287070c044b44d
Ridham Gupta <Ridhamkth@gmail.com>
2025-08-29 13:13:34 +0530

    Add Spring Security configuration and UserDetailsService implementation

--- src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/UserController.java
@@ -3,12 +3,15 @@ package net.engineeringdigest.journalApp.controller;
 
 import net.engineeringdigest.journalApp.entity.JournalEntry;
 import net.engineeringdigest.journalApp.entity.User;
+import net.engineeringdigest.journalApp.repository.UserRepository;
 import net.engineeringdigest.journalApp.service.JournalEntryService;
 import net.engineeringdigest.journalApp.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -21,26 +24,41 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping
-    public List<User> getAllUser(){
-         return userService.getAll();
-    }
+    @Autowired
+    private UserRepository userRepository;
 
-    @PostMapping
-    public void createUser(@RequestBody User user){
-        userService.saveEntry(user);
-    }
+    //once security is added we cant access all users. we can though create this function for admin
+//    @GetMapping
+//    public List<User> getAllUser(){
+//         return userService.getAll();
+//    }
 
-    @PutMapping("/{userName}")
-    public ResponseEntity<?> updateUser(@RequestBody User user,  @PathVariable String userName){
+
+
+    // createuser can be public but delete and updatte user cant be public it should require username and passward
+
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user){
+        //jb koi bhi user authenticate hota hae  to uski details store hoti hae securitycontextholder mae ,waha se le lenge hum.
+        Authentication authentication =SecurityContextHolder.getContext().getAuthentication();
+        String userName= authentication.getName();
         User userInDb = userService.findByUserName(userName);
-        if(userInDb!=null){
-            userInDb.setUserName(user.getUserName());
-            userInDb.setPassword(user.getPassword());
-            userService.saveEntry(userInDb);
-        }
+        userInDb.setUserName(user.getUserName());
+        userInDb.setPassword(user.getPassword());
+         userService.saveEntry(userInDb);
+
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
     }
 
+
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteUserById(){
+        Authentication authentication =SecurityContextHolder.getContext().getAuthentication();
+        userRepository.deleteByUserName(authentication.getName());
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+    }
 }
